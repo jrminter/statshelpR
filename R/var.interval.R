bdfb55e246c0210c76acab3989cde81bb4450a2b
John Minter <jrminter@gmail.com>
2018-05-19 15:59:43 -0400

    Now return a dataframe and properly handle different confidence intervals

--- R/var.interval.R
+++ R/var.interval.R
@@ -1,10 +1,17 @@
-#' Analyze the variance form a dataset using the chi**2 distribution
+#' Analyze the variance from a dataset using the chi**2 distribution
+#' 
+#' This was developed from [eda358.r](https://www.itl.nist.gov/div898/handbook/eda/section3/eda358.r)
+#' I modified the function to return a dataframe. The user can print
+#' the dataframe or use functions like kable to output a table in
+#' a Rmarkdown document. The original function supplied a default
+#' 95% confidence interval and let the user change it. However the 
+#' 95% CI was hardcoded in the body of the original function.
 #'
 #' @param data A vector of data values (real number)
 #' @param sigma0 The target standard deviation
 #' @param conf.level The desired confidence level (0.95)
 #' 
-#' @return None. Prints results
+#' @return A dataframe with results
 #' 
 #' @examples
 #' library(statshelpR)
@@ -17,7 +24,8 @@
 #'          0.424, 0.420, 0.424, 0.425, 0.425,
 #'          0.423, 0.431, 0.437, 0.422, 0.434)
 #'
-#' var.interval(dat, 0.002, 0.95)
+#' df <- var.interval(dat, 0.002, 0.95)
+#' print(df)
 #' 
 #' @export
 #'
@@ -29,16 +37,47 @@ var.interval = function(data,sigma0,conf.level = 0.95) {
   testchi = df*v/(sigma0^2)
   alpha = 1-conf.level
   
-  cat(paste("Standard deviation = ", round(sqrt(v),4)), "\n")
-  cat(paste("Test statistic = ", round(testchi,4)), "\n")
-  cat(paste("Degrees of freedom = ", round(df,0)),"\n")
-  cat("\n")
-  cat("Two-tailed test critical values, alpha=0.05","\n")
-  cat(paste("Lower = ", round(qchisq(alpha/2,df),4)),"\n")
-  cat(paste("Upper = ", round(qchisq(1-alpha/2,df),4)),"\n")
-  cat("\n")
-  cat("95% Confidence Interval for Standard Deviation","\n")
-  cat(c(round(sqrt(df * v/chiupper),4), 
-          round(sqrt(df * v/chilower),4)),"\n")
+  sAlpha <- round(alpha, 3)
+  sCI <- round(100*conf.level, 3)
+  
+  vNames <- c("Standard deviation",
+              "Test statistic",
+              "Degrees of freedom",
+              #              "",
+              paste("Two-tailed test critical values, alpha=", sAlpha),
+              "Lower T",
+              "Upper T",
+              #              "",
+              paste0(sCI, "% Confidence Interval for Standard Deviation"),
+              "Lower",
+              "Upper")
+  vResult <- c(paste(round(sqrt(v),4)),
+               paste(round(testchi,4)),
+               paste(round(df,0)),
+               #               "",
+               " ",
+               paste(round(qchisq(alpha/2,df),4)),
+               paste(round(qchisq(1-alpha/2,df),4)),
+               #               "",
+               " ",
+               round(sqrt(df * v/chiupper),4),
+               round(sqrt(df * v/chilower),4))
+  
+  df.out <- data.frame(Name=vNames,
+                       Value=vResult)
+  
+  return(df.out)
+  
+  #  cat(paste("Standard deviation = ", round(sqrt(v),4)), "\n")
+  #  cat(paste("Test statistic = ", round(testchi,4)), "\n")
+  #  cat(paste("Degrees of freedom = ", round(df,0)),"\n")
+  #  cat("\n")
+  #  cat("Two-tailed test critical values, alpha=0.05","\n")
+  #  cat(paste("Lower = ", round(qchisq(alpha/2,df),4)),"\n")
+  #  cat(paste("Upper = ", round(qchisq(1-alpha/2,df),4)),"\n")
+  #  cat("\n")
+  #  cat("95% Confidence Interval for Standard Deviation","\n")
+  #  cat(c(round(sqrt(df * v/chiupper),4), 
+  #        round(sqrt(df * v/chilower),4)),"\n")
 }
 
