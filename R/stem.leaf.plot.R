0b6ad5893f77174cd7e4c1515e2fddf8524ef90f
John Minter <jrminter@gmail.com>
2018-05-21 14:54:54 -0400

    added dtame and leaf pltot

---
+++ R/stem.leaf.plot.R
@@ -0,0 +1,36 @@
+#' Plot a Stem and Leaf plot
+#' 
+#' Plot a stem and leaf plot from a vector or list of observations
+#' 
+#' Credit: Adapted from https://stackoverflow.com/questions/26532564/how-to-output-a-stem-and-leaf-plot-as-a-plot
+#' 
+#' @param dat The vector of observations to plot
+#' 
+#' @return A ggplot
+#' 
+#' @import ggplot2
+#' 
+#' @export
+#' 
+#' @example
+#' library(statshelpR)
+#' print(stem.leaf.plot(iris$Petal.Length))
+#' 
+
+stem.leaf.plot <- function(dat){
+  tmp <- capture.output(stem(dat))
+  stemdf = data.frame(tmp, rr=1:length(tmp))
+  plt <- ggplot(stemdf) +
+         geom_text(aes(x=rr, y=0, label=tmp), hjust=0) + 
+         coord_flip() + theme_classic() + 
+         scale_x_discrete(breaks=NULL) + 
+         scale_y_discrete(breaks=NULL, limits=c(0,1))+ 
+         theme(axis.text = element_blank(),
+               axis.title = element_blank(), 
+               axis.ticks=element_blank(), 
+               panel.grid=element_blank(), 
+               axis.line=element_blank())
+         return(plt)
+}
+
+print(stem.leaf.plot(iris$Petal.Length))
