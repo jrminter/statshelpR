814271ae27efdebf1f55c51e6c1c2d54f961d464
John Minter <jrminter@gmail.com>
2018-05-29 17:49:30 -0400

    added a getting started vignette

---
+++ vignettes/getting-started.R
@@ -0,0 +1,4 @@
+## ---- echo=FALSE---------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, warning=FALSE)
+suppressWarnings(suppressMessages(suppressPackageStartupMessages(library(ggplot2))))
+
