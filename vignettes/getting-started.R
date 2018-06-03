bb904d9aeb2083c92423bacc85a8f3549eea3056
John Minter <jrminter@gmail.com>
2018-06-03 18:08:56 -0400

    fixed vignettes

--- vignettes/getting-started.R
+++
@@ -1,4 +0,0 @@
-## ---- echo=FALSE---------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, warning=FALSE)
-suppressWarnings(suppressMessages(suppressPackageStartupMessages(library(ggplot2))))
-
