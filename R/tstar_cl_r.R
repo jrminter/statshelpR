83d4d1a1291481b19e866849dd7de2d029d81850
John Minter <jrminter@gmail.com>
2018-06-07 18:20:30 -0400

    added data and vignette for tstar calc

---
+++ R/tstar_cl_r.R
@@ -0,0 +1,14 @@
+#' @name tstar_cl_r
+#' @aliases tstar_cl_r
+#' @title The confidence level for t* as a function value for the R qt function
+#' @description A dataframe the desired confidence level for finding the
+#'  t-star for a desired confidence level as a function of confidence
+#'  level entered into R's qt function.
+#' @docType data
+#' @usage data(tstar_cl_r)
+#' @format A data frame with 6 observations of the variable,
+#'  \code{CL} and the variable \code{R}.
+#' @source A table from http://www.stats4stem.org/r-t-distribution
+#' @author STATS4STEM under a Creative Commons licence
+NULL
+
