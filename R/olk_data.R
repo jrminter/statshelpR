a479018573c0de6935da1a77fdd21df644154ea1
John Minter <jrminter@gmail.com>
2018-06-07 22:11:01 -0400

    added data set from Krivanek

---
+++ R/olk_data.R
@@ -0,0 +1,15 @@
+#' @name olk_data
+#' @aliases olk_data
+#' @title Data for Thon Ring analysis
+#' @description A dataframe for linear regression analysis. It has the
+#'  following columns: n: the ring number, max: 1 if maximum, 0 if
+#'  minimum, s.recip.ang: the scattering vector in inverse Angstroms.
+#' @docType data
+#' @usage data(olk_data)
+#' @format A data frame with 8 observations of the variables
+#'  \code{n}, code{max}, and \code{s.recip.ang}.
+#' @source Data from Andre Krivanek on p.382 of Spence, High Resolution
+#'  Microscopy, 3rd Ed., Oxford Press (2003).
+#' @author Andre Krivanek. A standard data set.
+NULL
+
