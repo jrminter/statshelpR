d085c11f48e86fe21aac5e85949294d4d4e0c742
John Minter <jrminter@gmail.com>
2018-06-09 20:04:56 -0400

    added R/calc_z_star

---
+++ R/calc_z_star.R
@@ -0,0 +1,21 @@
+#' Calculate the z-star value for a confidence interval
+#' 
+#' We assume that the samples of interest are drawn from a Gaussian
+#' distribution. We want to compute the z-star value to compute the
+#' confidence interval for the desired mean'
+#' 
+#' @param p The probability of interest, e.g. 0.95
+#' 
+#' @return The z-score
+#' 
+#' @examples
+#' z_star <- calc_z_star(0.95)
+#' print(z_star)
+#' 
+#' @export
+#' 
+calc_z_star <- function(p){
+	x <- 0.5*(1.0-p)
+	z <- qnorm(x, lower.tail = FALSE)
+	return(z)
+}
